4b332a52bee2bfada676c3ed08968c4bec91d9fd
JARM21 <josuealejo200310@gmail.com>
2021-10-02 02:35:14 -0500

    Detalles de los Municipios

--- TorneoFutbol.App.Frontend/Pages/Municipios/Details.cshtml.cs
+++ TorneoFutbol.App.Frontend/Pages/Municipios/Details.cshtml.cs
@@ -4,13 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TorneoFutbol.App.Dominio;
+using TorneoFutbol.App.Persistencia;
 
 namespace TorneoFutbol.App.Frontend.Pages.Municipios
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioMunicipios _repoMunicipio;
+        public Municipio municipio {get; set;}
+        public DetailsModel(IRepositorioMunicipios repoMunicipio)
         {
+            _repoMunicipio = repoMunicipio;
+        }
+        public IActionResult OnGet(int id)
+        {
+            municipio = _repoMunicipio.GetMunicipio(id);
+            if (municipio == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
