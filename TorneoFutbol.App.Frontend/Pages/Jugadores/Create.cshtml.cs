86283d14d58de47e4a49259a9aab62d3b71053d5
JARM21 <josuealejo200310@gmail.com>
2021-10-09 09:19:21 -0500

    Creacion del Create  Jugadores

--- TorneoFutbol.App.Frontend/Pages/Jugadores/Create.cshtml.cs
+++ TorneoFutbol.App.Frontend/Pages/Jugadores/Create.cshtml.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TorneoFutbol.App.Dominio;
+using TorneoFutbol.App.Persistencia;
 
 namespace TorneoFutbol.App.Frontend.Pages.Jugadores
 {
     public class CreateModel : PageModel
     {
+        private readonly IRepositorioJugadores _repoJugador;
+        public Jugador jugador {get; set;}
+        public CreateModel(IRepositorioJugadores repoJugador)
+        {
+            _repoJugador = repoJugador;
+        }
         public void OnGet()
         {
+             jugador = new Jugador();
+        }
+
+        public IActionResult OnPost(Jugador jugador)
+        {
+            _repoJugador.AddJugador(jugador);
+            return RedirectToPage("Index");
         }
     }
 }
