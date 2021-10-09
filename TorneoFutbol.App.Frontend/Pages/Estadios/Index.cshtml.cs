4985a411b08f991cf4b251c2ee92b3568909d416
JARM21 <josuealejo200310@gmail.com>
2021-10-09 05:03:15 -0500

    conflictos del merge resueltos

--- TorneoFutbol.App.Frontend/Pages/Estadios/Index.cshtml.cs
+++ TorneoFutbol.App.Frontend/Pages/Estadios/Index.cshtml.cs
@@ -4,17 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-<<<<<<< HEAD
 using TorneoFutbol.App.Dominio;
 using TorneoFutbol.App.Persistencia;
-=======
->>>>>>> 14f503baa826c5b36d55961af0553170b41d8714
 
 namespace TorneoFutbol.App.Frontend.Pages.Estadios
 {
     public class IndexModel : PageModel
     {
-<<<<<<< HEAD
         private readonly IRepositorioEstadios _repoEstadio;
         public IEnumerable <Estadio> estadio {get; set;}
         public IndexModel(IRepositorioEstadios repoEstadio)
@@ -24,10 +20,6 @@ namespace TorneoFutbol.App.Frontend.Pages.Estadios
         public void OnGet()
         {
             estadio = _repoEstadio.GetAllEstadios();
-=======
-        public void OnGet()
-        {
->>>>>>> 14f503baa826c5b36d55961af0553170b41d8714
         }
     }
 }
