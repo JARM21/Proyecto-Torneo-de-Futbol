e11fea6e3719d23e9569eaff026f1b88a2b0942e
JARM21 <josuealejo200310@gmail.com>
2021-10-16 06:41:24 -0500

    Asiganar Equipo Visitante a Partidos

--- TorneoFutbol.App.Frontend/Pages/Partidos/AddEquipoVisitante.cshtml.cs
+++ TorneoFutbol.App.Frontend/Pages/Partidos/AddEquipoVisitante.cshtml.cs
@@ -4,13 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TorneoFutbol.App.Dominio;
+using TorneoFutbol.App.Persistencia;
 
 namespace TorneoFutbol.App.Frontend.Pages.Partidos
 {
     public class AddEquipoVisitanteModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioPartidos _repoPartido;
+        private readonly IRepositorioEquipos _repoEquipo;
+        public Partido partido {get; set;}
+        public IEnumerable<Equipo> equipo {get; set;}
+
+        public AddEquipoVisitanteModel(IRepositorioPartidos repoPartido, IRepositorioEquipos repoEquipo)
         {
+            _repoPartido = repoPartido;
+            _repoEquipo = repoEquipo;
+        }
+        public void OnGet(int Id)
+        {
+            partido = _repoPartido.GetPartido(Id);
+            equipo = _repoEquipo.GetAllEquipos();
+        }
+
+        public IActionResult OnPost(int IdPartido, int IdEquipo)
+        {
+            _repoPartido.AsignarEquipovisitante(IdPartido, IdEquipo);
+            return RedirectToPage("Details", new{Id = IdPartido});
         }
     }
 }
