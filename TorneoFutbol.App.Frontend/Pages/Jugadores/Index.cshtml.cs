029ff9baf12da2a3d8faf0ecab4a4e2072599d4a
JARM21 <josuealejo200310@gmail.com>
2021-10-16 11:17:58 -0500

    Diseño de la Pagina

--- TorneoFutbol.App.Frontend/Pages/Jugadores/Index.cshtml.cs
+++ TorneoFutbol.App.Frontend/Pages/Jugadores/Index.cshtml.cs
@@ -13,14 +13,13 @@ namespace TorneoFutbol.App.Frontend.Pages.Jugadores
     {
         private readonly IRepositorioJugadores _repoJugador;
         public IEnumerable <Jugador> jugador {get; set;}
+        
 
         public IndexModel(IRepositorioJugadores repoJugador)
         {
             _repoJugador = repoJugador;
         }
-        public void OnGet()
-        {
-            jugador = _repoJugador.GetAllJugador();
-        }
+     
+        
     }
 }
