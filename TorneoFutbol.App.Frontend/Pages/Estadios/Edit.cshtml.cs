08596be230ef3552bbf521d549ec0c1ca9d71b40
JARM21 <josuealejo200310@gmail.com>
2021-10-16 05:00:42 -0500

    Edit Estadios

--- TorneoFutbol.App.Frontend/Pages/Estadios/Edit.cshtml.cs
+++ TorneoFutbol.App.Frontend/Pages/Estadios/Edit.cshtml.cs
@@ -4,13 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TorneoFutbol.App.Dominio;
+using TorneoFutbol.App.Persistencia;
 
 namespace TorneoFutbol.App.Frontend.Pages.Estadios
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioEstadios _repoEstadio;
+        public Estadio estadio {get; set;}
+        public EditModel(IRepositorioEstadios repoEstadio)
         {
+            _repoEstadio = repoEstadio;
+        }
+        public void OnGet(int Id)
+        {
+            estadio = _repoEstadio.GetEstadio(Id);
+            if (estadio == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Page();
+            }
+        }
+
+        public IActionResult OnPost(Estadio estadio)
+        {
+            _repoEstadio.UpdateEstadio(estadio);
+            return RedirectToPage("Index");
         }
     }
 }
