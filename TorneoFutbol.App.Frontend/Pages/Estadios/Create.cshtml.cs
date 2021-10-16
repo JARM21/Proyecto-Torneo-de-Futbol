161fba9ffbd10164192bd22d9d0fb6bf9c8d29eb
JARM21 <josuealejo200310@gmail.com>
2021-10-16 04:50:51 -0500

    Create Estadios

--- TorneoFutbol.App.Frontend/Pages/Estadios/Create.cshtml.cs
+++ TorneoFutbol.App.Frontend/Pages/Estadios/Create.cshtml.cs
@@ -4,13 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TorneoFutbol.App.Dominio;
+using TorneoFutbol.App.Persistencia;
 
 namespace TorneoFutbol.App.Frontend.Pages.Estadios
 {
     public class CreateModel : PageModel
     {
+        private readonly IRepositorioEstadios _repoEstadio;
+        public Estadio estadio {get; set;}
+
+        public CreateModel(IRepositorioEstadios repoEstadio)
+        {
+            _repoEstadio = repoEstadio;
+        }
         public void OnGet()
         {
+            estadio = new Estadio();
+        }
+
+        public IActionResult OnPost(Estadio estadio)
+        {
+            if (ModelState.IsValid)
+            {
+                _repoEstadio.AddEstadio(estadio);
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
